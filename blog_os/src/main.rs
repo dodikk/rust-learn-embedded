13325bd640a64ade9706892b2b48e0d7286d2328
Alexander Dodatko <alexander.dodatko@dataart.com>
2018-07-03 17:06:16 +0300

    fixed "write trait not recognized" error
    error[E0599]: no method named `write_fmt` found for type
    `vga_buffer::Writer` in the current scope

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -4,7 +4,34 @@
 #![feature(panic_implementation)]
 
 
+// error[E0468]: an `extern crate` loading macros must be at the crate root
+//
+//
+
+#[macro_use]
+extern crate lazy_static;
+
+
+
+
+
+
+
+
 use core::panic::PanicInfo;
+
+
+extern crate spin;
+use spin::Mutex;
+
+
+use core::fmt;
+use core::fmt::{Write};
+
+
+mod vga_buffer;
+use vga_buffer::{Writer};
+
 
 /// This function is called on panic.
 #[panic_implementation]
@@ -24,16 +51,31 @@ pub fn panic(_info: &PanicInfo) -> !
 
 static HELLO: &[u8] = b"Hello World!";
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn print_abstractionless()
+{
     let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    for (i, &byte) in HELLO.iter().enumerate() 
+    {
+        unsafe 
+        {
+            *vga_buffer.offset(i as isize * 2    ) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb ;
         }
     }
+
+}
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! 
+{
+	// ::vga_buffer::print_something();
+
+	use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+
 
     loop {}
 }
