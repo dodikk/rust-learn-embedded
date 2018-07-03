da3a5c9b658263773a004167e106a09cb684ab4c
Alexander Dodatko <alexander.dodatko@dataart.com>
2018-07-03 14:25:31 +0300

    added extra command to install tools and switch to nightly rust

--- blog_os/build-bootable-x86.sh
+++ blog_os/build-bootable-x86.sh
@@ -1,3 +1,14 @@
 
-export XARGO_RUST_SRC=/usr/local/Cellar/rust/1.26.2/share/rust/rust_src/
+
+# export XARGO_RUST_SRC=/usr/local/Cellar/rust/1.26.2/share/rust/rust_src/
+
+
+
+
+# == once == 
+#
+# cargo install cargo-xbuild
+# rustup component add rust-src
+#
+#
 cargo xbuild --target x86_64-blog_os.json
--- blog_os/build-standalone.sh
+++ blog_os/build-standalone.sh
@@ -1,3 +1,15 @@
+
+
+# curl -s https://static.rust-lang.org/rustup.sh > rustup.sh
+# sh -s -- --channel=nightly
+# rustup default nightly
+#
+# $ rustc --version
+### rustc 1.28.0-nightly (e3bf634e0 2018-06-28)
+
+
+
+
  cargo rustc -- -Z pre-link-arg=-lSystem
 
 ## cargo rustc -- -Z pre-link-arg=-nostartfiles
