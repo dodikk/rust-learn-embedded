0d1ad6ffd27f3bda9c18a448cc04ebf76a2e809e
Alexander Dodatko <alexander.dodatko@dataart.com>
2018-07-03 13:45:17 +0300

    managed to build the binary

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+
+#![no_std]
+#![no_main]
+#![feature(panic_implementation)]
+
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! 
+{
+    loop {}
 }
+
+
+//======= Rust (Linux ??)
+//
+//
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! 
+{
+    loop {}
+}
+
+
+
+//======= Mac OS
+//
+//
+
+#[no_mangle]
+pub extern "C" fn main() -> ! 
+{
+    loop {}
+}
+
+
+//======= Windows
+//
+//
+
+#[no_mangle]
+pub extern "C" fn mainCRTStartup() -> ! {
+    main();
+}
+
+// #[no_mangle]
+// pub extern "C" fn main() -> ! 
+// {
+//     loop {}
+// }
+
